c9d8e7cf834fb5417fd5fe8729215d3b5fb58030
Tom Herman <tomherman4@gmail.com>
2023-06-27 00:18:15 +0300

    add "run-fj-file" skeleton

--- src/bf2fj.py
+++ src/bf2fj.py
@@ -1,5 +1,6 @@
-from definitions import *
+from pathlib import Path
 
+from definitions import BRAINFUCK_DIR, FLIPJUMP_DIR
 from bf2fj_compiler import Bf2FjCompiler
 
 
@@ -11,12 +12,21 @@ FIXED_INPUT = b''
 EXPECTED_OUTPUT = b'Hello World!\n'
 
 
-def main():
+def run_fj_and_verify_expected_output(flipjump_code_path: Path, fixed_input: bytes, expected_output: bytes) -> bool:
+    pass
+
+
+def main() -> None:
     with open(BRAINFUCK_PROGRAM_PATH, 'r') as bf_file:
-        compiler = Bf2FjCompiler(bf_file.read())
+        bf_code = bf_file.read()
+
+    compiler = Bf2FjCompiler(bf_code)
+    fj_code = compiler.get_compiled_code()
 
     with open(FLIPJUMP_PROGRAM_PATH, 'w') as fj_file:
-        fj_file.write(compiler.get_compiled_code())
+        fj_file.write(fj_code)
+
+    run_fj_and_verify_expected_output(FLIPJUMP_PROGRAM_PATH, FIXED_INPUT, EXPECTED_OUTPUT)
 
 
 if __name__ == '__main__':
--- src/bf2fj_compiler.py
+++ src/bf2fj_compiler.py
@@ -2,6 +2,6 @@ class Bf2FjCompiler:
     def __init__(self, brainfuck_code: str):
         self.brainfuck_code = brainfuck_code
 
-    def get_compiled_code(self):
+    def get_compiled_code(self) -> str:
         # TODO implement
         return ''

5a886c20e93e2d547f4326cdac0acd05c804b87f
Tom Herman <tomherman4@gmail.com>
2023-07-07 23:22:49 +0300

    skeleton for check fj input/output
    Needs to update fj to be used as a package..

--- src/bf2fj.py
+++ src/bf2fj.py
@@ -1,15 +1,20 @@
+import argparse
 import shlex
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
+from flipjump.src import fjm, assembler, fjm_run
 from flipjump.src.defs import PrintTimer
-from flipjump.src.fj import assemble_run_according_to_cmd_line_args
+from flipjump.src.fj import assemble_run_according_to_cmd_line_args, get_file_tuples
 
 from definitions import BRAINFUCK_DIR, FLIPJUMP_DIR
 from bf2fj_compiler import Bf2FjCompiler
+from flipjump.tests.test_fj import test_run, RunTestArgs, CSV_TRUE, DEBUGGING_FILE_SUFFIX
 
+fjm_width = 32
 
 DEBUG_FLAGS: str = '-d --debug-ops-list 1000'
-GENERAL_FLAGS: str = '-w 32'
+GENERAL_FLAGS: str = f'-w {fjm_width}'
 
 
 BRAINFUCK_PROGRAM_PATH: Path = BRAINFUCK_DIR / 'hello_world.bf'
@@ -27,13 +32,39 @@ def run_fj_and_verify_expected_output(flipjump_code_path: Path, fixed_input: byt
     :param expected_output: The expected output the fj-code-file would output given the fixed_input input.
     :return: True if the file's output matched the expected-output, while the input was fixed-input.
     """
-    assemble_run_according_to_cmd_line_args(cmd_line_args=shlex.split(
-        f'"{flipjump_code_path}" {DEBUG_FLAGS} {GENERAL_FLAGS} -o "{flipjump_code_path}m" -v 0'
-    ))
+    
+    with TemporaryDirectory as tmp_dir:
+        tmp_dir_path = Path(tmp_dir)
+        fjm_file_path = tmp_dir_path / f'{flipjump_code_path.stem}.fjm'
+        debugging_file_path = tmp_dir_path / f'{fjm_file_path.name}{DEBUGGING_FILE_SUFFIX}'
+        in_file_path = tmp_dir_path / f'fixed_input.txt'
+        out_file_path = tmp_dir_path / f'expected_output.txt'
 
-    # TODO use fixed input as input, and assert that the output is exactly expected_output.
-    #  Can be done by using  flipjump.src.assembler.assemble(..)  and  flipjump.src.fjm_run.run(..)  directly.
-    return False
+        with open(in_file_path, 'wb') as in_file:
+            in_file.write(fixed_input)
+        with open(out_file_path, 'wb') as out_file:
+            out_file.write(expected_output)
+
+        file_tuples = get_file_tuples(argparse.Namespace(no_stl=False, files=[flipjump_code_path]))
+
+        fjm_writer = fjm.Writer(fjm_file_path, fjm_width, fjm.RelativeJumpVersion)   # TODO use compressed when available
+        assembler.assemble(file_tuples, fjm_width, fjm_writer,
+                           debugging_file_path=debugging_file_path)
+
+        try:
+            test_run(RunTestArgs(
+                save_debug_file=True, debug_info_length=100, test_name=flipjump_code_path.stem,
+                fjm_path=str(fjm_file_path.absolute()),
+                in_file_path=str(in_file_path.absolute()), out_file_path=str(out_file_path.absolute()),
+                read_in_as_binary__str=CSV_TRUE, read_out_as_binary__str=CSV_TRUE))
+            return True
+        except AssertionError as e:
+            print(e)
+            return False
+
+    # assemble_run_according_to_cmd_line_args(cmd_line_args=shlex.split(
+    #     f'"{flipjump_code_path}" {DEBUG_FLAGS} {GENERAL_FLAGS} -o "{flipjump_code_path}m" -v 0'
+    # ))
 
 
 def compile_brainfuck_file_to_flipjump_file(brainfuck_file_path: Path, flipjump_file_path: Path) -> None:

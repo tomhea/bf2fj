6ce3a7f6d0eb462e7627315475f731b95928a5cd
Tomhe <tomherman4@gmail.com>
2023-09-08 12:22:30 +0300

    use flipjump as a package

--- src/bf2fj.py
+++ src/bf2fj.py
@@ -1,21 +1,25 @@
-import argparse
-import shlex
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from flipjump.src import fjm, assembler, fjm_run
-from flipjump.src.defs import PrintTimer
-from flipjump.src.fj import assemble_run_according_to_cmd_line_args, get_file_tuples
-
 from definitions import BRAINFUCK_DIR, FLIPJUMP_DIR
 from bf2fj_compiler import Bf2FjCompiler
-from flipjump.tests.test_fj import test_run, RunTestArgs, CSV_TRUE, DEBUGGING_FILE_SUFFIX
+
+from flipjump.debugging.breakpoints import BreakpointHandler, load_labels_dictionary
+from flipjump.fj import get_file_tuples
+from flipjump.interpretter import fjm_run
+from flipjump.io_devices.FixedIO import FixedIO
+from flipjump.utils.classes import TerminationCause, PrintTimer
+from flipjump.assembler import assembler
+
+from flipjump.fjm.fjm_consts import RelativeJumpVersion
+from flipjump.fjm.fjm_writer import Writer
 
 fjm_width = 32
 
 DEBUG_FLAGS: str = '-d --debug-ops-list 1000'
 GENERAL_FLAGS: str = f'-w {fjm_width}'
-
+DEBUG_INFO_LENGTH = 1000
+EXPECTED_TERMINATION_CAUSE = TerminationCause.Looping
 
 BRAINFUCK_PROGRAM_PATH: Path = BRAINFUCK_DIR / 'hello_world.bf'
 FLIPJUMP_PROGRAM_PATH: Path = FLIPJUMP_DIR / 'hello_world.fj'
@@ -24,47 +28,40 @@ FIXED_INPUT: bytes = b''
 EXPECTED_OUTPUT: bytes = b'Hello World!\n'
 
 
-def run_fj_and_verify_expected_output(flipjump_code_path: Path, fixed_input: bytes, expected_output: bytes) -> bool:
+def run_fj_and_verify_expected_output(flipjump_code_path: Path, fixed_input: bytes, expected_output: bytes) -> None:
     """
     Run the given flip-jump program, give it the fixed-input, and check if the output is as expected.
     :param flipjump_code_path: A path to a (non-compiled) flip-jump code file.
     :param fixed_input: This will be the input to the fj-code-file.
-    :param expected_output: The expected output the fj-code-file would output given the fixed_input input.
-    :return: True if the file's output matched the expected-output, while the input was fixed-input.
+    :param expected_output: The expected output the fj-code-file should output given the fixed_input input.
+    :raise AssertionError: if running the code generates different output then the expected-output.
     """
     
     with TemporaryDirectory() as tmp_dir:
         tmp_dir_path = Path(tmp_dir)
         fjm_file_path = tmp_dir_path / f'{flipjump_code_path.stem}.fjm'
-        debugging_file_path = tmp_dir_path / f'{fjm_file_path.name}{DEBUGGING_FILE_SUFFIX}'
-        in_file_path = tmp_dir_path / f'fixed_input.txt'
-        out_file_path = tmp_dir_path / f'expected_output.txt'
+        debugging_file_path = tmp_dir_path / f'{fjm_file_path.name}.fj_debugging_info'
 
-        with open(in_file_path, 'wb') as in_file:
-            in_file.write(fixed_input)
-        with open(out_file_path, 'wb') as out_file:
-            out_file.write(expected_output)
+        # assemble
+        file_tuples = get_file_tuples([str(flipjump_code_path.absolute())], no_stl=False)
+        fjm_writer = Writer(fjm_file_path, fjm_width, RelativeJumpVersion)   # TODO use compressed when available
+        assembler.assemble(file_tuples, fjm_width, fjm_writer, debugging_file_path=debugging_file_path)
 
-        file_tuples = get_file_tuples(argparse.Namespace(no_stl=False, files=[flipjump_code_path]))
+        io_device = FixedIO(fixed_input)
+        label_to_address = load_labels_dictionary(debugging_file_path, True)
+        breakpoint_handler = BreakpointHandler({}, {label_to_address[label]: label
+                                               for label in tuple(label_to_address)[::-1]})
 
-        fjm_writer = fjm.Writer(fjm_file_path, fjm_width, fjm.RelativeJumpVersion)   # TODO use compressed when available
-        assembler.assemble(file_tuples, fjm_width, fjm_writer,
-                           debugging_file_path=debugging_file_path)
+        termination_statistics = fjm_run.run(fjm_file_path,
+                                             io_device=io_device,
+                                             time_verbose=True,
+                                             last_ops_debugging_list_length=DEBUG_INFO_LENGTH,
+                                             breakpoint_handler=breakpoint_handler)
 
-        try:
-            test_run(RunTestArgs(
-                save_debug_file=True, debug_info_length=100, test_name=flipjump_code_path.stem,
-                fjm_path=str(fjm_file_path.absolute()),
-                in_file_path=str(in_file_path.absolute()), out_file_path=str(out_file_path.absolute()),
-                read_in_as_binary__str=CSV_TRUE, read_out_as_binary__str=CSV_TRUE))
-            return True
-        except AssertionError as e:
-            print(e)
-            return False
-
-    # assemble_run_according_to_cmd_line_args(cmd_line_args=shlex.split(
-    #     f'"{flipjump_code_path}" {DEBUG_FLAGS} {GENERAL_FLAGS} -o "{flipjump_code_path}m" -v 0'
-    # ))
+        termination_statistics.print(labels_handler=breakpoint_handler,
+                                     output_to_print=io_device.get_output(allow_incomplete_output=True))
+        assert termination_statistics.termination_cause == EXPECTED_TERMINATION_CAUSE
+        assert expected_output == io_device.get_output()
 
 
 def compile_brainfuck_file_to_flipjump_file(brainfuck_file_path: Path, flipjump_file_path: Path) -> None:

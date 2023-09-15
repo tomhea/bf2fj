55044562a95b973f2494e120e5e51866b1aecdf8
Tomhe <tomherman4@gmail.com>
2023-09-15 18:10:50 +0300

    split assemble-run-test fj into a function

--- src/bf2fj.py
+++ src/bf2fj.py
@@ -18,6 +18,19 @@ FIXED_INPUT: bytes = b''
 EXPECTED_OUTPUT: bytes = b'Hello World!\n'
 
 
+def run_fj_and_verify_expected_output(flipjump_code_path: Path, fixed_input: bytes, expected_output: bytes) -> None:
+    """
+    Run the given flip-jump program, give it the fixed-input, and check if the output is as expected.
+    :param flipjump_code_path: A path to a (non-compiled) flip-jump code file.
+    :param fixed_input: This will be the input to the fj-code-file.
+    :param expected_output: The expected output the fj-code-file should output given the fixed_input input.
+    :raise AssertionError: if running the code generates different output then the expected-output.
+    """
+    assemble_and_run_test_output([flipjump_code_path], fixed_input, expected_output,
+                                 should_raise_assertion_error=True,
+                                 w=fjm_width, last_ops_debugging_list_length=DEBUG_INFO_LENGTH)
+
+
 def compile_brainfuck_file_to_flipjump_file(brainfuck_file_path: Path, flipjump_file_path: Path) -> None:
     """
     Compile the given brainfuck file into a flip-jump file.
@@ -37,8 +50,7 @@ def compile_brainfuck_file_to_flipjump_file(brainfuck_file_path: Path, flipjump_
 
 def main() -> None:
     compile_brainfuck_file_to_flipjump_file(BRAINFUCK_PROGRAM_PATH, FLIPJUMP_PROGRAM_PATH)
-    assemble_and_run_test_output([FLIPJUMP_PROGRAM_PATH], FIXED_INPUT, EXPECTED_OUTPUT,
-                                 w=fjm_width, last_ops_debugging_list_length=DEBUG_INFO_LENGTH)
+    run_fj_and_verify_expected_output(FLIPJUMP_PROGRAM_PATH, FIXED_INPUT, EXPECTED_OUTPUT)
 
 
 if __name__ == '__main__':
